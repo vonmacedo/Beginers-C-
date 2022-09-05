a05d3d40ac73727ce604499502899a03f087fcfa
João Victor Macedo <112831848+vonmacedo@users.noreply.github.com>
2022-09-04 21:52:01 -0300

    Add files via upload

---
+++ Prj_37450_37458_Ex01/Prj_37450_37458_Ex01/C37450_37458@Exec01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prj_37450_37458_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA = 0, valorB = 0, valorR = 0;
+            Console.Write("Digite a base");
+            valorA = int.Parse(Console.ReadLine());
+            Console.Write("Digite a altura");
+            valorB = int.Parse(Console.ReadLine());
+            valorR = valorA * valorB;
+            Console.Write("O resultado deu:" + valorR.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prj_37450_37458_Exerc02/Prj_37450_37458_Exerc2/C37450_37458@exerc02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prj_37450_37458_Exerc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA = 0, valorB = 0, valorC = 0;
+            Console.Write("Digite a primeira aresta");
+            valorA = int.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda aresta");
+            valorB = int.Parse(Console.ReadLine());
+            valorC = valorA * valorB;
+            Console.Write("O resultado deu:" + valorC.ToString());
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
